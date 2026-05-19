4693a24e16d6ae6f27dd26e2b7b56064a97d965b
Isaac dev <isaaclimab78@gmail.com>
2026-05-19 13:17:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+javascript
+function scrollToMenu() {
+  document.getElementById('menu').scrollIntoView({
+    behavior: 'smooth'
+  });
+}
 
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.transform = 'translateY(-10px)';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'translateY(0px)';
+  });
+});
